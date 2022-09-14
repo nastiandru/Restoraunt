afe45db6546fcfe94fd07a6a805ecade7dd6198e
nastiandru <nastia.andr070703gmail.com>
2022-09-15 01:33:57 +0300

    Create OrderModel.js

---
+++ Models/OrderModel.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["Submitted"] = 0] = "Submitted";
+    OrderStatus[OrderStatus["InProgress"] = 1] = "InProgress";
+    OrderStatus[OrderStatus["Done"] = 2] = "Done";
+    OrderStatus[OrderStatus["BillIssued"] = 3] = "BillIssued";
+})(OrderStatus || (OrderStatus = {}));
+var Order = /** @class */ (function () {
+    function Order(employee, items, status, table, price) {
+        this.employee = employee;
+        this.items = items;
+        this.status = status;
+        this.table = table;
+        this.price = price;
+    }
+    return Order;
+}());
+exports["default"] = Order;
