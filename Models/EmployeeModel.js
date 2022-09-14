23fef396f5714ca1f799d4e4a80ce337f0595c86
nastiandru <nastia.andr070703gmail.com>
2022-09-15 00:26:27 +0300

    Create EmployeeModel.js

---
+++ Models/EmployeeModel.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var Employee = /** @class */ (function () {
+    function Employee(employeeId, name, surname, position, restaurant, restaurantName) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        this.restaurantName = restaurantName;
+    }
+    return Employee;
+}());
+exports["default"] = Employee;
