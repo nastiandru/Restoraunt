8af1756aea20f4408b90f88b26f3813250771801
nastiandru <nastia.andr070703gmail.com>
2022-09-15 02:40:46 +0300

    Update EmployeeRepository.ts

--- DataStore/EmployeeRepository.ts
+++ DataStore/EmployeeRepository.ts
@@ -6,11 +6,10 @@ export class EmployeeRepository
 {
     employeeSchema = new Schema<Employee>(
         {
-            employeeId: {type: Number, required: true},
             name: {type: String, required: true},
             surname: {type: String, required: true},
             position: {type: String, required: true},
-            restaurantName: {type: String, required: true}
+            restaurant: {type: Schema.Types.ObjectId, ref: 'Restaurant', required: true}
         });
 
     EmployeeModel = model<Employee>('Employee', this.employeeSchema);
@@ -22,33 +21,32 @@ export class EmployeeRepository
         const employees =
         [
             {
-                employeeId: 1,
                 name: 'Employee1',
                 surname: 'Employee1',
                 position: 'Manager',
-                restaurantName: 'Restaurant1'
+                restaurant: '62825d046e2b74b56f5ccc77'
             },
             {
-                employeeId: 2,
                 name: 'Employee2',
                 surname: 'Employee2',
                 position: 'Waiter',
-                restaurantName: 'Restaurant1'
+                restaurant: '62825d046e2b74b56f5ccc77'
             },
             {
                 employeeId: 3,
                 name: 'Employee3',
                 surname: 'Employee3',
                 position: 'Waiter',
-                restaurantName: 'Restaurant2'
+                restaurant: '62825d046e2b74b56f5ccc77'
             },
             {
                 employeeId: 4,
                 name: 'Employee4',
                 surname: 'Employee4',
                 position: 'Manager',
-                restaurantName: 'Restaurant2'
+                restaurant: '62825d046e2b74b56f5ccc77'
             }
+
         ];
 
         if(await this.EmployeeModel.countDocuments() === 0)
@@ -69,79 +67,38 @@ export class EmployeeRepository
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
 
-        let restaurantRepository = new RestaurantRepository();
-
-        let resturantExists: boolean = await restaurantRepository.CheckIfRestaurantExists(employee.restaurantName);
-
-        if(resturantExists)
-        {
-            await this.EmployeeModel
-            .create(employee)
-            .then(function()
-            {
-                console.log("Employee " + employee.name + " has been added!");
-            }).catch(function(err: any)
-            {
-                console.log(err);
-            });
-        }
-        else
-        {
-            console.log("Restaurant " + employee.restaurantName + " does not exist!");
-        }
-    }
-
-    async deleteEmployeeById(employeeId: string) : Promise<void>
-    {
-        await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-
         await this.EmployeeModel
-        .deleteOne({employeeId: employeeId})
+        .create(employee)
         .then(function()
         {
-            console.log("Employee " + employeeId + " has been deleted!");
+            console.log("Employee " + employee.surname + " has been added!");
         }).catch(function(err: any)
         {
             console.log(err);
         });
     }
 
-    async deleteEmployeeBySurname(employeeName: string) : Promise<void>
+    async deleteEmployeeBySurname(employeeSurname: string) : Promise<void>
     {
         await connect('mongodb+srv://username:username123@cluster.itsrg.mongodb.net/RestaurantDb?retryWrites=true&w=majority');
 
 
         await this.EmployeeModel
-        .deleteOne({name: employeeName})
+        .deleteOne({surname: employeeSurname})
         .then(function()
         {
-            console.log("Employee " + employeeName + " has been deleted!");
+            console.log("Employee " + employeeSurname + " has been deleted!");
         }).catch(function(err: any)
         {
             console.log(err);
         });
     }
 
-    async getEmployeeById(employeeId: string) : Promise<Employee>
-    {
-        await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-
-        let employee = await this.EmployeeModel.findOne({employeeId: employeeId});
-        if(employee)
-        {
-            return employee;
-        }
-        else
-        {
-            return null as any;
-        }
-    }
-
-    async getEmployeeBySurname(employeeName: string) : Promise<Employee>
+    async getEmployeeBySurname(employeeSurname: string) : Promise<Employee>
     {
         await connect('mongodb+srv://username:username123@cluster.itsrg.mongodb.net/RestaurantDb?retryWrites=true&w=majority');
 
-        let employee = await this.EmployeeModel.findOne({name: employeeName});
+        let employee = await this.EmployeeModel.findOne({surname: employeeSurname});
         if(employee)
         {
             return employee;
@@ -167,11 +124,11 @@ export class EmployeeRepository
         }
     }
 
-    async getEmployeesByRestaurantName(restaurantName: string) : Promise<Employee[]>
+    async getEmployeesByRestaurantId(restaurantId: Schema.Types.ObjectId) : Promise<Employee[]>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
 
-        let employees = await this.EmployeeModel.find({restaurantName: restaurantName});
+        let employees = await this.EmployeeModel.find({restaurant: restaurantId});
         if(employees)
         {
             return employees;
@@ -187,10 +144,10 @@ export class EmployeeRepository
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
 
         await this.EmployeeModel
-        .updateOne({employeeId: employee.employeeId}, employee)
+        .updateOne(employee)
         .then(function()
         {
-            console.log("Employee " + employee.employeeId + " has been updated!");
+            console.log("Employee " + employee.surname + " has been updated!");
         }).catch(function(err: any)
         {
             console.log(err);
