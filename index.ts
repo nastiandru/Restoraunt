a7df03b041fb59af20239a13690a6ff5e4bb53ba
nastiandru <nastia.andr070703gmail.com>
2022-09-14 22:16:56 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const Restaurant = require('./Models/RestaurantModel');
+const {Restaurant} = require('./Models/RestaurantModel');
 import express = require ('express');
+import bodyParser = require('body-parser');
 import {Request, Response} from 'express';
 
 
@@ -10,29 +11,42 @@ import { RestaurantRepository } from "./DataStore/RestaurantRepository";
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', router);
+
 const restaurantRepository = new RestaurantRepository();
 
 router.get('/restaurants', async (req: Request, res: Response) => {
     let restaurants = await restaurantRepository.getRestaurants();
-    if (restaurants) {
+    if (restaurants.length > 0)
         res.json(restaurants);
-    } else {
+        else if (restaurants.length == 0)
+        res.status(200).send("Restaurant list is empty");
+    else
         res.status(404).send("No restaurants found");
     }
-});
+);
 
 router.get('/restaurant/:name', async (req: Request, res: Response) => {
-    let restaurant = await restaurantRepository.getRestaurantByName(req.params.name);
+    const restaurant = await restaurantRepository.getRestaurantByName(req.params.name);
     if (restaurant)
-    {
-        res.json(restaurant);
-    }
+    res.json(restaurant);
     else
-    {
-        res.status(404).send("Restaurant not found");
-    }
+    res.status(404).send("Restaurant not found");
+
 });
 
-app.use('/', router);
+router.delete('/restaurant/:name', async (req: Request, res: Response) => {
+        await restaurantRepository.deleteRestaurantByName(req.params.name);
+        res.status(200).send("Restaurant deleted");
+    });
+    
+router.post('/restaurant', async (req: Request, res: Response) => {
+        const restaurant = req.body;
+        await restaurantRepository.addRestaurant(restaurant);
+        res.status(200).send("Restaurant added");
+    });
+   
 
 app.listen(3004);
