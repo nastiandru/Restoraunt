abd52a1cebf0fb508d250815499778806ad8eb41
nastiandru <nastia.andr070703gmail.com>
2022-09-14 22:59:24 +0300

    Update CustomerModel.ts

--- Models/CustomerModel.ts
+++ Models/CustomerModel.ts
@@ -1,13 +1,15 @@
 class Customer
 {
+    customerId: Number;
     name: string;
     email: string;
     phone: string;
     address: string;
     loyaltyPoints: number;
 
-    constructor(name: string, email: string, phone: string, address: string, loyaltyPoints: number;)
+    constructor(customerId: Number, name: string, email: string, phone: string, address: string, loyaltyPoints: number)
     {
+        this.customerId = customerId;
         this.name = name;
         this.email = email;
         this.phone = phone;
