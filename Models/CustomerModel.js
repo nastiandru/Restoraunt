cd28adedfc0f6cd589a0abcff8af72b3abe41394
nastiandru <nastia.andr070703gmail.com>
2022-09-15 00:26:28 +0300

    Create CustomerModel.js

---
+++ Models/CustomerModel.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var Customer = /** @class */ (function () {
+    function Customer(customerId, name, email, phone, address, loyaltyPoints) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+    return Customer;
+}());
+exports["default"] = Customer;
