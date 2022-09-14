6baf1b74388905bf2b0606c8a220bc1f9d45d7b0
nastiandru <nastia.andr070703gmail.com>
2022-09-14 20:09:33 +0300

    Create RestaurantModel.js

---
+++ Models/RestaurantModel.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var Restaurant = /** @class */ (function () {
+    function Restaurant(name, address, phone, nip, email, website) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.nip = nip;
+        this.email = email;
+        this.website = website;
+    }
+    return Restaurant;
+}());
+exports["default"] = Restaurant;
