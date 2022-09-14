9cbea570f81a0360ee007bc2c9f9e0768b0f60af
nastiandru <nastia.andr070703gmail.com>
2022-09-14 20:09:48 +0300

    Create MenuItem.ts

---
+++ Models/MenuItem.ts
@@ -0,0 +1,30 @@
+import Product from './ProductModel';
+
+enum Type
+{
+    Starter,
+    MainDish,
+    SideDish,
+    Drink,
+    Dessert
+}
+
+class MenuItem
+{
+    name: string;
+    price: number;
+    type: Type;
+    description: string;
+    products: Product[];
+
+    constructor(name: string, price: number, type: Type, description: string, products: Product[])
+    {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.description = description;
+        this.products = products;
+    }
+}
+
+export default MenuItem;
