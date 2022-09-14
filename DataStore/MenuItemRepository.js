039b65b3ae96cc6758bedc8a62f97948cd25a554
nastiandru <nastia.andr070703gmail.com>
2022-09-15 00:26:54 +0300

    Create MenuItemRepository.js

---
+++ DataStore/MenuItemRepository.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.MenuItemRepository = void 0;
+var mongoose_1 = require("mongoose");
+var MenuItemRepository = /** @class */ (function () {
+    function MenuItemRepository() {
+        this.MenuItemSchema = new mongoose_1.Schema({
+            menuItemId: { type: Number, required: true },
+            name: { type: String, required: true },
+            price: { type: Number, required: true },
+            type: { type: Number, required: true },
+            description: { type: String, required: true },
+            products: 
+        });
+    }
+    return MenuItemRepository;
+}());
+exports.MenuItemRepository = MenuItemRepository;
