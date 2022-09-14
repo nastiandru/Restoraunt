dfcf545ba98a54d0feb2c1fb5abf211e3bf03d18
nastiandru <nastia.andr070703gmail.com>
2022-09-14 19:24:58 +0300

    Create ReservationModel.ts

---
+++ Models/ReservationModel.ts
@@ -0,0 +1,20 @@
+import Table from './TableModel';
+import Customer from './CustomerModel';
+
+class Reservation
+{
+    table: Table;
+    startDateTime: Date;
+    endDateTime: Date;
+    customer: Customer;
+
+    constructor(table: Table, startDateTime: Date, endDateTime: Date, customer: Customer)
+    {
+        this.table = table;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.customer = customer;
+    }
+}
+
+export default Reservation;
