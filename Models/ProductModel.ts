4133b5c27204d19a5c5733ad7cbc1f9b4cc554c0
nastiandru <nastia.andr070703gmail.com>
2022-09-15 02:40:40 +0300

    Update ProductModel.ts

--- Models/ProductModel.ts
+++ Models/ProductModel.ts
@@ -1,27 +1,18 @@
-enum Unit
-{
-    piece,
-    g,
-    ml,
-    kg,
-    l
-}
 
 class Product
 {
-    productId: number;
+   
     name: string;
     price: number;
     quantity: number;
-    unit: Unit;
-
-    constructor( productId: number, name: string, price: number, quantity: number, unit: Unit)
+    
+    constructor( name: string, price: number, quantity: number,)
     {
-        this.productId = productId;
+        
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.unit = unit;
+        
     }
 }
 
