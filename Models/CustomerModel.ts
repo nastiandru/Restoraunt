ac2096039c89c6acece01d8086572ec3135b5790
nastiandru <nastia.andr070703gmail.com>
2022-09-14 18:46:56 +0300

    Create CustomerModel.ts

---
+++ Models/CustomerModel.ts
@@ -0,0 +1,18 @@
+class Customer
+{
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+
+    constructor(name: string, email: string, phone: string, address: string)
+    {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+}
+
+export default Customer;
