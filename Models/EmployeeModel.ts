72922227e3e0d336b204f88925ada7d7891adf15
nastiandru <nastia.andr070703gmail.com>
2022-09-14 18:46:55 +0300

    Create EmployeeModel.ts

---
+++ Models/EmployeeModel.ts
@@ -0,0 +1,15 @@
+class Employee 
+{
+    name: string;
+    surname: string;
+    position: string
+
+    constructor(name: string, surname: string, position: string)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+    }
+}
+
+export default Employee;
