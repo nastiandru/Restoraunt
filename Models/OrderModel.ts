aa468fc970b22084f444df99378fbd38eecf553a
nastiandru <nastia.andr070703gmail.com>
2022-09-15 07:58:23 +0300

    Update OrderModel.ts

--- Models/OrderModel.ts
+++ Models/OrderModel.ts
@@ -12,14 +12,16 @@ enum OrderStatus
 
 class Order
 {
+    dateTime: Date;
     employee: Employee;
     items: MenuItem[];
     status: OrderStatus;
     table: Table;
     price: number;
 
-    constructor(employee: Employee, items: MenuItem[], status: OrderStatus, table: Table, price: number)
+    constructor(dateTime: Date,employee: Employee, items: MenuItem[], status: OrderStatus, table: Table, price: number)
     {
+        this.dateTime = dateTime;
         this.employee = employee;
         this.items = items;
         this.status = status;
