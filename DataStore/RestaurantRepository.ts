5b12f2b40b392bf9a5326c0b7b2648f03e2f496e
nastiandru <nastia.andr070703gmail.com>
2022-09-15 06:20:38 +0300

    Update RestaurantRepository.ts

--- DataStore/RestaurantRepository.ts
+++ DataStore/RestaurantRepository.ts
@@ -10,6 +10,7 @@ export class RestaurantRepository
             nip: {type: String, required: true},
             email: {type: String, required: true},
             website: {type: String, required: true},
+            description: {type: String, required: false}
            
         });
 
@@ -28,14 +29,7 @@ export class RestaurantRepository
                 email: 'someEmail@something.com',
                 website: 'someWebsite.com'
             },
-            {
-                name: 'Restaurant2',
-                address: 'Address2',
-                phone: '987654321',
-                nip: '987654321',
-                email: 'someEmail@somethingElse.com',
-                website: 'someOtherWebsite.com'
-            }];
+        ];
             
             if(await this.RestaurantModel.countDocuments() === 0)
         {
@@ -43,7 +37,7 @@ export class RestaurantRepository
             .insertMany(restaurants)
             .then(function()
             {
-                console.log("Restaurants have been populated!")
+                console.log('Restaurants have been populated!')
             }).catch(function(err)
             {
                 console.log(err);
@@ -51,55 +45,75 @@ export class RestaurantRepository
         }
     }
 
-    async addRestaurant(restaurant: Restaurant) : Promise<void>
+    async addRestaurant(restaurant: Restaurant) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
+
+        const alreadyExists = await this.RestaurantModel.findOne({name: restaurant.name});
+        if(alreadyExists)
+            return false;
 
         await this.RestaurantModel
         .create(restaurant)
         .then(function()
         {
-            console.log("Restaurant" + restaurant.name + " has been added!")
+            console.log('Restaurant ' + restaurant.name + ' has been added!');
         }).catch(function(err)
         {
             console.log(err);
         });
+
+        
+        const existsAfter = await this.RestaurantModel.findOne({name: restaurant.name});
+        if (existsAfter)
+            return true;
+        else
+            return false;
     }
 
-    async deleteRestaurantByName(restaurantName: string) : Promise<void>
+    async deleteRestaurantByName(restaurantName: string) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
+        
+        const exists = await this.RestaurantModel.exists({name: restaurantName});
+        if (!exists)
+            return false;
+
         await this.RestaurantModel
         .deleteOne({name: restaurantName})
         .then(function()
         {
-            console.log("Restaurant" + restaurantName + " has been deleted!")
+            console.log('Restaurant ' + restaurantName + ' has been deleted!');
         }).catch(function(err)
         {
             console.log(err);
         });
+        return true;
     }
-    async getRestaurantByName(restaurantName: string) : Promise<Restaurant>
+
+    async getRestaurantByName(restaurantName: string) : Promise<Restaurant | boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-        let restaurant = await this.RestaurantModel.findOne({name: restaurantName});
+        const restaurant = await this.RestaurantModel.findOne({name: restaurantName});
         if (restaurant)
-        
             return restaurant;
         
         else
-        
-            return null as any;
+            return false;
         
     }
 
-    async getRestaurants() : Promise<Restaurant[]>
+    async getRestaurants() : Promise<Restaurant[] | boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-        return await this.RestaurantModel.find({});
+        const restaurants = await this.RestaurantModel.find({});
+        if(restaurants)
+            return restaurants;
+        else
+            return false;
     }
 
-    async updateRestaurant(restaurantName: string, restaurant: Restaurant) : Promise<void>
+    async updateRestaurant(restaurantName: string, restaurant: Restaurant) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
 
@@ -118,28 +132,24 @@ export class RestaurantRepository
             restaurantToUpdate.email = restaurant.email;
         if(restaurant.website)
             restaurantToUpdate.website = restaurant.website;
+            if(restaurant.description)
+            restaurantToUpdate.description = restaurant.description
+
 
         await this.RestaurantModel
         .updateOne({name: restaurantName}, restaurantToUpdate)
         .then(function()
         {
-            console.log("Restaurant " + restaurantName + " has been updated!");
+            console.log('Restaurant ' + restaurantName + ' has been updated!');
         }).catch(function(err)
         {
             console.log(err);
         });
+        return true;
         }
         else
-        console.log("Restaurant " + restaurantName + " does not exist!");
+        {
+        return false;
         }
-
-    async CheckIfRestaurantExists(restaurantName: string) : Promise<boolean>
-    {
-        await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-        let restaurant = await this.RestaurantModel.findOne({name: restaurantName});
-        if (restaurant)
-            return true;
-        else
-            return false;
     }
 }
