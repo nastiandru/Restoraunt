9ebfbeccd3781cab7dfaa4c670a2c9e4f734ca83
nastiandru <nastia.andr070703gmail.com>
2022-09-15 03:40:42 +0300

    Update ProductModel.js

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,20 +1,10 @@
 "use strict";
 exports.__esModule = true;
-var Unit;
-(function (Unit) {
-    Unit[Unit["piece"] = 0] = "piece";
-    Unit[Unit["g"] = 1] = "g";
-    Unit[Unit["ml"] = 2] = "ml";
-    Unit[Unit["kg"] = 3] = "kg";
-    Unit[Unit["l"] = 4] = "l";
-})(Unit || (Unit = {}));
 var Product = /** @class */ (function () {
-    function Product(productId, name, price, quantity, unit) {
-        this.productId = productId;
+    function Product(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.unit = unit;
     }
     return Product;
 }());
