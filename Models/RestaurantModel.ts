890cbe82283a79b70bc606ea188a6a04c42b531b
nastiandru <nastia.andr070703gmail.com>
2022-09-15 06:20:37 +0300

    Update RestaurantModel.ts

--- Models/RestaurantModel.ts
+++ Models/RestaurantModel.ts
@@ -5,8 +5,10 @@ class Restaurant{
     nip: string;
     email: string;
     website: string;
+    description?: string
 
-    constructor(name: string, address: string, phone: string, nip: string, email: string, website: string)
+
+    constructor(name: string, address: string, phone: string, nip: string, email: string, website: string, description?: string)
     {
         this.name = name;
         this.address = address;
@@ -14,6 +16,7 @@ class Restaurant{
         this.nip = nip;
         this.email = email;
         this.website = website;
+        this.description = description;
     }
 }
 
