38a72bb186ede1b1ec3a7ee3a5d2b9282cd62857
nastiandru <nastia.andr070703gmail.com>
2022-09-15 06:20:46 +0300

    Update CustomerRepository.ts

--- DataStore/CustomerRepository.ts
+++ DataStore/CustomerRepository.ts
@@ -51,11 +51,16 @@ export class CustomerRepository
         }
     }
 
-    async addCustomer(customer: Customer) : Promise<void>
+    async addCustomer(customer: Customer) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
         
-        customer.loyaltyPoints = 0; // set default value for loyalty points before saving => loyaltyPoints are required in the schema
+        const alreadyExists = await this.CustomerModel.findOne({name: customer.name});
+        if(alreadyExists)
+            return false;
+
+        if(!customer.loyaltyPoints)
+            customer.loyaltyPoints = 0; // set default value for loyalty points before saving => loyaltyPoints are required in the schema
 
         await this.CustomerModel
         .create(customer)
@@ -67,11 +72,21 @@ export class CustomerRepository
         {
             console.log(err);
         });
+
+        const existsAfter = await this.CustomerModel.findOne({name: customer.name});
+        if(existsAfter)
+            return true;
+        else
+            return false;
     }
 
-    async deleteCustomerByName(customerName: string) : Promise<void>
+    async deleteCustomerByName(customerName: string) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
+
+        const exists = await this.CustomerModel.exists({name: customerName});
+        if(!exists)
+            return false;
 
         await this.CustomerModel
         .deleteOne({name: customerName})
@@ -83,25 +98,36 @@ export class CustomerRepository
         {
             console.log(err);
         });
+        const existsAfter = await this.CustomerModel.exists({name: customerName});
+        if(!existsAfter)
+            return true;
+        else
+        return false;
     }
 
-    async getCustomerByName(customerName: string) : Promise<Customer>
+    async getCustomerByName(customerName: string) : Promise<Customer | boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-        let customer = await this.CustomerModel.findOne({name: customerName});
+
+        const customer = await this.CustomerModel.findOne({name: customerName});
         if (customer)
             return customer;
         else
-            return null as any;
+        return false;
+ 
     }
 
-    async getCustomers() : Promise<Customer[]>
+    async getCustomers() : Promise<Customer[] | boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
-        return await this.CustomerModel.find({});
+        const customers = await this.CustomerModel.find({});
+        if (customers.length > 0)
+            return customers;
+        else
+            return false;
     }
 
-    async updateCustomer(customerName:string, customer: Customer) : Promise<void>
+    async updateCustomer(customerName:string, customer: Customer) : Promise<boolean>
     {
         await connect('mongodb+srv://nastia123:nastia070703@cluster0.eyf7qte.mongodb.net/?retryWrites=true&w=majority');
 
@@ -129,14 +155,13 @@ export class CustomerRepository
             {
                 console.log(err);
             });
+            return true;
         }
         else
-        {
-            console.log("Customer " + customerName + " does not exist!");
-        }
+        return false;
     }
 
-    async addLoyaltyPoints(customerName: string, loyaltyPoints: number) : Promise<void>
+    async addLoyaltyPoints(customerName: string, loyaltyPoints: number) : Promise<boolean>
     {
         await connect('mongodb+srv://username:username123@cluster.itsrg.mongodb.net/RestaurantDb?retryWrites=true&w=majority');
         
@@ -149,12 +174,15 @@ export class CustomerRepository
             .updateOne({name: customerName}, customer)
             .then(function()
             {
-                console.log("Customer loyalty points have been added to " + customerName + "!")
+                console.log(loyaltyPoints + " loyalty points have been added to " + customerName + "!")
             }
             ).catch(function(err: any)
             {
                 console.log(err);
             });
+            return true;
+        }
+        else
+        return false;
         }
     }
-}
