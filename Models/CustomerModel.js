1c77396a700793f3fc0c86cd84d9512b7eedafb2
nastiandru <nastia.andr070703gmail.com>
2022-09-15 03:41:15 +0300

    Update CustomerModel.js

--- Models/CustomerModel.js
+++ Models/CustomerModel.js
@@ -1,8 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var Customer = /** @class */ (function () {
-    function Customer(customerId, name, email, phone, address, loyaltyPoints) {
-        this.customerId = customerId;
+    function Customer(name, email, phone, address, loyaltyPoints) {
         this.name = name;
         this.email = email;
         this.phone = phone;
