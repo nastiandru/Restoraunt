9424a1de5bdb0cd7bbaa4e97d39a8c24430c3afb
nastiandru <nastia.andr070703gmail.com>
2022-09-15 08:00:12 +0300

    auto js

--- Models/RestaurantModel.js
+++ Models/RestaurantModel.js
@@ -1,13 +1,14 @@
 "use strict";
 exports.__esModule = true;
 var Restaurant = /** @class */ (function () {
-    function Restaurant(name, address, phone, nip, email, website) {
+    function Restaurant(name, address, phone, nip, email, website, description) {
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.nip = nip;
         this.email = email;
         this.website = website;
+        this.description = description;
     }
     return Restaurant;
 }());
